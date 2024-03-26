1e2ec57e198aea404389a54bbb460cb835858e2b
Administrator <pavel.eiiasp@gmail.com>
2024-03-26 20:32:49 +0100

    this is new function.

---
+++ Myfproject1.py
@@ -0,0 +1,24 @@
+print("test")
+print("test2")
+import math
+cislo = 55
+delitel = 51
+print(math.floor(cislo / delitel))
+print("tohle je dvojita uvozovka=\n new row")
+#print(cislo // delitel)
+#print(cislo % delitel)
+print(5<=6)
+a = 5
+b = 6
+print (a == b)
+print ( a!= b)
+if a < b :
+  print ("pravda")
+  print ("ano")
+  a =5
+  b =6
+  if a<b:
+    print("pravda"+"ve")
+    vyska = 150
+    vaha = 80
+print (vyska/vaha)
